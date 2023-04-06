9d5bc1d6c5436d0c4a92eaece32f69729220fa2f
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-04-06 12:20:07 +0500

    30-03-2023

---
+++ index.js
@@ -0,0 +1,132 @@
+// alert("Calculator")
+
+// function add(){
+//     let result=num1+num2
+//     alert("Sum is "+result)
+// }
+// function sub(){
+//     let result=num1-num2
+//     alert("Subtraction is "+result)
+// }
+// function mul(){
+//     let result=num1*num2
+//     alert("Multiplication is "+result)
+// }
+// function div(){
+//     if(num2===0){
+//         alert("Cannot divide by zero")
+//     }
+//     else{
+//     let result=num1/num2
+//     alert("Division is "+result)
+// }
+// }
+
+// // let user=prompt("Enter + to add. Enter - to subtract. Enter * to multiply. Enter / to divide.")
+// let user=prompt("+, - , *, /")
+// let num1=+prompt("Enter first number")
+// let num2=+prompt("Enter second number")
+
+
+// if(user==="+"){
+//     add()
+// }
+// else if(user==="-"){
+//     sub()
+// }
+// else if(user==="*"){
+//     mul()
+// }
+// else if(user=== "/"){
+//     div()
+// }
+// else{
+//     alert("Invalid Input")
+// }
+
+// function display()
+// {
+
+//     let user=prompt("Please Enter your name")
+//     alert(user)
+
+// }
+
+// display()
+
+
+
+
+// function add(num1,num2){
+//   let sum=num1+num2
+//    alert(sum)
+// }
+
+// let n1=+prompt("Enter first number")
+// let n2=+prompt("Enter second number")
+
+// add(n1,n2)
+
+
+// let salary=10000
+
+// function increment(sal){
+//    sal=salary+5000
+//     alert("Your Ramzan Salary is: "+sal)
+// }
+// increment(salary)
+// alert("Your normal salary is: "+salary)
+
+
+
+
+
+// function fullName(first,last=""){
+//     alert("Your full name is: "+first+" "+last)
+// }
+
+
+// let firstName=prompt("What`s your first name?")
+// let lastName=prompt("What`s your last name?")
+// fullName(firstName)
+
+
+// function add(num1,num2){
+//   let sum=num1+num2
+//  return sum
+// }
+
+// let n1=+prompt("Enter first number")
+// let n2=+prompt("Enter second number")
+
+// let answer=add(n1,n2)
+// answer*=2
+// alert(answer)
+
+
+
+
+// function userInput(){
+//     let howMany=+prompt("Enter how many values you want to enter")
+//     let arr=[]
+//     for(let i=0;i<howMany;i++){
+//          arr[i]=+prompt("Enter number")
+        
+//     }
+// return arr
+// }
+
+
+// let arr1=userInput()
+// document.write(arr1)
+
+// function sum(arr){
+//     let total=0
+//     for(let i=0;i<arr.length;i++){
+//         total+=arr[i]
+//     }
+//    document.write("<br>"+total)
+// }
+
+
+// sum(arr1)
